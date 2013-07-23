50e5f2dc88d41a160f9c26374c23c34237a60f7e
Manuel de la Pena <manuel@canonical.com>
2013-07-23 19:55:51 +0200

    Fixed all lint errors.

--- ubuntu-download-manager-tests/fake_request_factory.cpp
+++ ubuntu-download-manager-tests/fake_request_factory.cpp
@@ -17,33 +17,30 @@
  */
 
 #include <QDebug>
-#include "fake_network_reply.h"
-#include "fake_request_factory.h"
+#include "./fake_network_reply.h"
+#include "./fake_request_factory.h"
 
 RequestWrapper::RequestWrapper(const QNetworkRequest& request, QObject* parent)
     : QObject(parent),
-    _request(request)
-{
+    _request(request) {
 }
 
-QNetworkRequest RequestWrapper::request()
-{
+QNetworkRequest
+RequestWrapper::request() {
     return _request;
 }
 
-FakeRequestFactory::FakeRequestFactory(QObject *parent) :
-    RequestFactory(parent),
-    Fake()
-{
+FakeRequestFactory::FakeRequestFactory(QObject *parent)
+    : RequestFactory(parent),
+      Fake() {
 }
 
-NetworkReply* FakeRequestFactory::get(const QNetworkRequest& request)
-{
+NetworkReply*
+FakeRequestFactory::get(const QNetworkRequest& request) {
     // return a FakeQNetworkReply
     FakeNetworkReply* reply = new FakeNetworkReply();
 
-    if (_recording)
-    {
+    if (_recording) {
         QList<QObject*> inParams;
         inParams.append(new RequestWrapper(request));
 
