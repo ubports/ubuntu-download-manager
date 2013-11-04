3d2af6106a7605dccfa3bf705a1ca438cc76eacf
Manuel de la Pena <manuel@canonical.com>
2013-11-04 12:45:17 +0100

    Fix destructor issues.

--- libubuntudownloadmanager/downloads/state_machines/download_sm.cpp
+++ libubuntudownloadmanager/downloads/state_machines/download_sm.cpp
@@ -271,6 +271,8 @@ class DownloadSMPrivate {
         delete _downloadingSslErrorTransition;
         delete _downloadingReconnectTransition;
         delete _downloadingNotConnectedCanceled;
+        delete _pauseResumeTransition;
+        delete _pausedCancelTransition;
         delete _idle;
         delete _init;
         delete _downloading;
