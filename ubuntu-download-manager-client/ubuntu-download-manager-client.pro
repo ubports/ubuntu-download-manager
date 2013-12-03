e7c66b5f0ca0e34a18eb4705e834a7918075f3e0
Manuel de la Pena <manuel@canonical.com>
2013-12-03 17:41:58 +0100

    Added first little parts for the client lib.

--- ubuntu-download-manager-client/ubuntu-download-manager-client.pro
+++ ubuntu-download-manager-client/ubuntu-download-manager-client.pro
@@ -11,8 +11,28 @@ DEFINES += UBUNTUDOWNLOADMANAGERCLIENT_LIBRARY
 
 SOURCES += ubuntu/download_manager/manager.cpp
 
-HEADERS += ubuntu/download_manager/manager.h\
-        ubuntu-download-manager-client_global.h
+public_headers = \
+    ubuntu/download_manager/manager.h
+
+private_headers = \
+    ubuntu-download-manager-client_global.h
+
+HEADERS +=\
+    $$public_headers \
+    $$private_headers
 
 OTHER_FILES += \
     ubuntu-download-manager-client.pc.in
+
+headers.files = $$public_headers
+
+include(../common-installs-config.pri)
+
+pkgconfig.files = ubuntu-download-manager-client.pc
+include(../common-pkgconfig.pri)
+INSTALLS += pkgconfig
+
+LIBS += -L$$OUT_PWD/../ubuntu-download-manager-common/ -lubuntu-download-manager-common
+
+INCLUDEPATH += $$PWD/../ubuntu-download-manager-common
+DEPENDPATH += $$PWD/../ubuntu-download-manager-common
--- ubuntu-download-manager-client/ubuntu/download_manager/manager.h
+++ ubuntu-download-manager-client/ubuntu/download_manager/manager.h
@@ -1,13 +1,64 @@
-#ifndef MANAGER_H
-#define MANAGER_H
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
+#ifndef UBUNTU_DOWNLOADMANAGER_CLIENT_MANAGER_H
+#define UBUNTU_DOWNLOADMANAGER_CLIENT_MANAGER_H
+
+#include <functional>
+#include <QObject>
+#include <ubuntu/download_manager/download_struct.h>
+#include <ubuntu/download_manager/group_download_struct.h>
 #include "ubuntu-download-manager-client_global.h"
 
-class UBUNTUDOWNLOADMANAGERCLIENTSHARED_EXPORT Manager
-{
-    
-public:
-    Manager();
+namespace Ubuntu {
+
+namespace DownloadManager {
+
+class Download;
+class GroupDownload;
+
+typedef std::function<void(Download*)> DownloadCreationCb;
+typedef std::function<void(GroupDownload*)> GroupCreationCb;
+
+class ManagerPrivate;
+class UBUNTUDOWNLOADMANAGERCLIENTSHARED_EXPORT Manager : public QObject {
+    Q_DECLARE_PRIVATE(Manager)
+    Q_OBJECT
+
+ public:
+    explicit Manager(QObject* parent);
+
+    virtual Download* createDownload(DownloadStruct downStruct);
+    virtual void createDownload(DownloadStruct downStrcut, DownloadCreationCb cb);
+    virtual GroupDownload* createDownload(GroupDownloadStruct  groupStruct);
+    virtual void createDownload(GroupDownloadStruct  groupStruct, GroupCreationCb cb);
+
+ signals:
+    void downloadCreated(Download* down);
+    void groupCreated(GroupDownload* down);
+
+ private:
+    // use pimpl pattern so that users do not have to be recompiled
+    ManagerPrivate* d_ptr;
 };
 
-#endif // MANAGER_H
+}  // DownloadManager
+
+}  // Ubuntu
+
+#endif  // UBUNTU_DOWNLOADMANAGER_CLIENT_MANAGER_H
