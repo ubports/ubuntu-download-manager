da6cc04964d224201eb2119ec466943eddbd92f5
Manuel de la Pena <manuel@canonical.com>
2014-02-10 10:59:47 +0100

    Fix go bindings license header.

--- bindings/golang/udm.go
+++ bindings/golang/udm.go
@@ -10,7 +10,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * usensord is distributed in the hope that it will be useful,
+ * ubuntu-download-manager is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
