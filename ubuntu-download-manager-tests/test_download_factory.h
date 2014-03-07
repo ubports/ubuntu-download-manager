723a8a9579c4387e5b40eb380f30e311034b7cea
Manuel de la Peña <manuel.delapena@canonical.com>
2014-03-07 19:25:10 +0100

    Moved to single executables for the daemon tests.

---
+++ ubuntu-download-manager-tests/test_download_factory.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013-2014 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef TEST_DOWNLOAD_FACTORY_H
+#define TEST_DOWNLOAD_FACTORY_H
+
+#include <QObject>
+#include <ubuntu/downloads/factory.h>
+#include <ubuntu/download_manager/tests/server/apparmor.h>
+#include <ubuntu/download_manager/tests/server/system_network_info.h>
+#include <ubuntu/download_manager/tests/server/request_factory.h>
+#include <ubuntu/download_manager/tests/server//process_factory.h>
+#include <ubuntu/download_manager/tests/server/uuid_factory.h>
+#include <ubuntu/download_manager/tests/base_testcase.h>
+#include <ubuntu/download_manager/tests/test_runner.h>
+
+using namespace Ubuntu::DownloadManager;
+using namespace Ubuntu::DownloadManager::Daemon;
+
+class TestDownloadFactory : public BaseTestCase {
+    Q_OBJECT
+
+ public:
+    explicit TestDownloadFactory(QObject *parent = 0);
+
+ private slots:  // NOLINT(whitespace/indent)
+
+    void init() override;
+    void cleanup() override;
+
+    void testCreateDownload();
+    void testCreateDownloadWithHash();
+    void testCreateMmsDownload();
+    void testCreateGroupDownload();
+    void testCreateDownloadWithValidUuid();
+    void testCreateDownloadWithNullUuid();
+    void testCreateDownloadWithHashAndUuid();
+    void testCreateDownloadWithHashAndNullUuid();
+    void testCreateGroupDownloadWithValidUuid();
+    void testCreateGroupDownloadWithNullUuid();
+    void testCreateDownloadForGroup();
+    void testCreateDownloadForGroupWithHash();
+
+ private:
+    FakeAppArmor* _apparmor;
+    SystemNetworkInfo* _networkInfo;
+    ProcessFactory* _processFactory;
+    QSharedPointer<UuidFactory> _uuidFactory;
+    Factory* _downFactory;
+};
+
+#endif  // TEST_DOWNLOAD_FACTORY_H
---
+++ ubuntu-download-manager-tests/test_network_error_transition.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013-2014 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef TEST_NETWORK_ERROR_TRANSITION_H
+#define TEST_NETWORK_ERROR_TRANSITION_H
+
+#include <QFinalState>
+#include <QState>
+#include <QStateMachine>
+#include <QObject>
+#include <ubuntu/downloads/state_machines/download_sm.h>
+#include <ubuntu/download_manager/tests/base_testcase.h>
+#include <ubuntu/download_manager/tests/server/sm_file_download.h>
+
+using namespace Ubuntu::DownloadManager::Daemon::StateMachines;
+
+class TestNetworkErrorTransition : public BaseTestCase {
+    Q_OBJECT
+ public:
+    explicit TestNetworkErrorTransition(QObject *parent = 0);
+    
+ private slots:  // NOLINT(whitespace/indent)
+    void init() override;
+    void cleanup() override;
+    void testOnTransition_data();
+    void testOnTransition();
+
+ private:
+    QStateMachine _stateMachine;
+    FakeSMFileDownload* _down;
+    NetworkErrorTransition* _transition;
+    QState* _s1;
+    QFinalState* _s2;
+};
+
+#endif  // TEST_NETWORK_ERROR_TRANSITION_H
