29d899deff0c176af1529785f63673ae50cd229e
Michael Sheldon <michael.sheldon@canonical.com>
2015-10-20 15:05:37 +0100

    Add missing download history files

---
+++ src/downloads/qml/download_history.cpp
@@ -0,0 +1,99 @@
+#include "download_history.h"
+#include <glog/logging.h>
+#include <ubuntu/download_manager/download_struct.h>
+#include <QCoreApplication>
+#include <QDebug>
+
+namespace Ubuntu {
+
+namespace DownloadManager {
+
+DownloadHistory::DownloadHistory(QObject *parent) :
+    QObject(parent),
+    m_manager(nullptr)
+{
+    m_manager = Manager::createSessionManager("", this);
+
+    // Get previous downloads for this app
+    auto environment = QProcessEnvironment::systemEnvironment();
+    if (environment.contains("APP_ID")) {
+        m_manager->getUncollectedDownloads(environment.value("APP_ID"));
+    } else {
+        m_manager->getUncollectedDownloads(QCoreApplication::applicationFilePath());
+    }
+
+    CHECK(connect(m_manager, &Manager::downloadsFound,
+        this, &DownloadHistory::downloadsFound))
+            << "Could not connect to signal";
+}
+
+DownloadHistory *DownloadHistory::instance()
+{
+    static DownloadHistory *downloadHistory = new DownloadHistory();
+    return downloadHistory;
+}
+
+QVariantList DownloadHistory::downloads() const
+{
+    return m_downloads;
+}
+
+void DownloadHistory::addDownload(SingleDownload *singleDownload)
+{
+    m_downloads.append(QVariant::fromValue(singleDownload));
+    emit downloadsChanged();
+}
+
+void DownloadHistory::downloadsFound(DownloadsList* downloadsList)
+{
+    foreach(QSharedPointer<Download> download, downloadsList->downloads()) {
+        SingleDownload* singleDownload = new SingleDownload(this);
+        CHECK(connect(singleDownload, &SingleDownload::finished,
+            this, &DownloadHistory::downloadCompleted))
+                << "Could not connect to signal";
+        singleDownload->bindDownload(download.data());
+        m_downloads.append(QVariant::fromValue(singleDownload));
+    }
+    qDebug() << "Got downloads: " << m_downloads;
+    emit downloadsChanged();
+    qDebug() << "Huh?";
+}
+
+bool DownloadHistory::cleanDownloads() const
+{
+    return m_cleanDownloads;
+}
+
+void DownloadHistory::setCleanDownloads(bool value)
+{
+    m_cleanDownloads = value;
+    if (m_cleanDownloads) {
+        QVariantList newList;
+        foreach(QVariant var, m_downloads) {
+            SingleDownload *download = qobject_cast<SingleDownload*>(var.value<SingleDownload*>());
+            if (download != nullptr && !download->isCompleted()) {
+                newList.append(QVariant::fromValue(download));
+            } else {
+                download->deleteLater();
+            }
+        }
+        m_downloads = newList;
+        emit downloadsChanged();
+    }
+}
+
+void DownloadHistory::downloadCompleted()
+{
+    if (m_cleanDownloads) {
+        SingleDownload* download = qobject_cast<SingleDownload*>(sender());
+        if (download != nullptr) {
+            int index = m_downloads.indexOf(QVariant::fromValue(download));
+            m_downloads.removeAt(index);
+            emit downloadsChanged();
+            download->deleteLater();
+        }
+    }
+}
+
+}
+}
