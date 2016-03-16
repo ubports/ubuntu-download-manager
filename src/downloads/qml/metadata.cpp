c05c77b6b2dbfa60013d78a143057452f6fa4d8b
Michael Sheldon <michael.sheldon@canonical.com>
2016-03-16 16:03:39 +0000

    Document metadata component

--- src/downloads/qml/metadata.cpp
+++ src/downloads/qml/metadata.cpp
@@ -22,6 +22,76 @@ namespace Ubuntu {
 
 namespace DownloadManager {
 
+/*!
+    \qmltype Metadata
+    \instantiates Metadata
+    \inqmlmodule Ubuntu.DownloadManager 1.2
+    \ingroup download
+    \brief Provides additional properties for downloads
+
+    Metadata provides the ability to set a number of special
+    properties on a SingleDownload, e.g. to display a download in
+    the transfer indicator, or automatically extract a download
+    of a zip file upon completion.
+
+    Example usage:
+
+    \qml
+    import QtQuick 2.0
+    import Ubuntu.Components 1.2
+    import Ubuntu.DownloadManager 1.2
+
+    Rectangle {
+        width: units.gu(100)
+        height: units.gu(20)
+
+        TextField {
+            id: text
+            placeholderText: "File URL to download..."
+            height: 50
+            anchors {
+                left: parent.left
+                right: button.left
+                rightMargin: units.gu(2)
+            }
+        }
+
+        Button {
+            id: button
+            text: "Download"
+            height: 50
+            anchors.right: parent.right
+
+            onClicked: {
+                single.download(text.text);
+            }
+        }
+
+        ProgressBar {
+            minimumValue: 0
+            maximumValue: 100
+            value: single.progress
+            anchors {
+                left: parent.left
+                right: parent.right
+                bottom: parent.bottom
+            }
+
+            SingleDownload {
+                id: single
+                metadata: Metadata {
+                    showInIndicator: true
+                    title: "Example download"
+                }
+            }
+        }
+    }
+    \endqml
+
+    \sa {DownloadManager}
+*/
+
+
 Metadata::Metadata(QObject* parent)
     : QObject(parent) {
 }
@@ -87,6 +157,30 @@ QVariantMap
 Metadata::map() const {
     return QVariantMap(_metadata);
 }
+
+
+/*!
+    \qmlproperty bool Metadata::showInIndicator
+
+    Specifies whether this download should be displayed in the transfers
+    indicator, defaults to False.
+*/
+
+/*!
+    \qmlproperty string Metadata::title
+
+    This sets the name to display in the transfer indicator for this
+    download, this property is only used if showInIndicator is True.
+*/
+
+
+/*!
+    \qmlproperty bool Metadata::extract
+
+    When set to True the download manager will attempt to automatically 
+    extract zip files when the download finishes. This property defaults
+    to False.
+*/
 
 }
 
